1622d04faf4e3b935a09aa5b141ea54cb57b1a36
dieudoanhut2002 <127190623+dieudoanhut2002@users.noreply.github.com>
2024-08-26 17:21:15 +0700

    Add APIService Project, test API working flow

---
+++ ABCBook.API/Controllers/BookController.cs
@@ -0,0 +1,21 @@
+﻿using ABCBook.Model.Support;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ABCBook.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class BookController : Controller
+    {
+        private readonly IBookService _bookService;
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+        [HttpGet]
+        public Task<APIResult<Guid>> Create()
+        {
+            return _bookService.Create(Guid.NewGuid());
+        }
+    }
+}
---
+++ ABCBookAPIService/API/Service/BookService.cs
@@ -0,0 +1,13 @@
+﻿using ABCBook.Model.Support;
+
+namespace ABCBookAPIService.API.Service
+{
+    public class BookService : IBookService
+    {
+        public async Task<APIResult<Guid>> Create(Guid id)
+        {
+            APIResult<Guid> result = new();
+            return result;
+        }
+    }
+}
